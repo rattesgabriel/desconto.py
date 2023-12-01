bd678b48f90880dd6164fde0e9d184cfce868357
rattesgabriel <152656243+rattesgabriel@users.noreply.github.com>
2023-12-01 17:55:42 -0300

    Add files via upload

---
+++ desconto.py
@@ -0,0 +1,7 @@
+v = input('Valor inicial: ')
+d = input('valor do desconto: ')
+c = float(d) /100
+b = float(v) * float(c) 
+o = float(v) - float(b)
+print (f'o Valor Final com desconto será de: {o} seu puto!')
+ 
